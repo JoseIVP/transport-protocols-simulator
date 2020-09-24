6329b9e6a2f583867a1e06dec24606e01bca27db
Jose Vergara <vergarajose365@gmail.com>
2020-09-23 23:07:08 -0300

    Added tests for the receive() method of SRSender
    Added two tests that currently are not passing as the
    method is not implemented.

--- js/tests/selectiveRepeatTest.js
+++ js/tests/selectiveRepeatTest.js
@@ -133,14 +133,122 @@ describe("selectiveRepeat.js", function(){
         });
 
         describe("#receive()", function(){
+            let channel,
+            receiver,
+            sender,
+            receivedOkAcks,
+            receivedNotOkAcks,
+            sentPackets,
+            acksToSend;
 
-            it("should receive acknowledgments for each packet");
+            beforeEach(function(){
+                channel = new Channel({delay: 1000});
+                receiver = new Node();
+                sender = new SRSender({
+                    channel,
+                    receiver,
+                    windowSize: 3,
+                    timeout: 2100
+                });
+                receivedOkAcks = [];
+                receivedNotOkAcks = [];
+                sentPackets = [];
+                acksToSend = [];
+                sender.onSend = packet => {
+                    sentPackets.push(packet);
+                };
+                sender.onReceive = (packet, channel, isOk) => {
+                    if(isOk) receivedOkAcks.push(packet);
+                    else receivedNotOkAcks.push(packet);
+                }
+                let i = 0;
+                receiver.onReceive = () => {
+                    channel.send(acksToSend[i]);
+                    i++;
+                };
+            });
 
-            it("should move the window when the oldest packet is acknowledged");
+            it("should unset the timeouts for correctly received acknowledgments", function(done){
+                this.timeout(2500);
+                // We will send 3 packets and only correctly acknowledge the second.
+                // The sender should then resend sequence numbers 0 and 2, and should
+                // have unset the timeout for sequence number 1.
+                acksToSend.push(new Packet({
+                    isAck: true,
+                    receiver: sender,
+                    ackNum: 0
+                }));
+                acksToSend[0].getCorrupted();
+                acksToSend.push(new Packet({
+                    isAck: true,
+                    receiver: sender,
+                    ackNum: 1
+                }));
+                acksToSend.push(new Packet({
+                    isAck: true,
+                    receiver: sender,
+                    ackNum: 1
+                }));
+                sender.send();
+                sender.send();
+                sender.send();
+                setTimeout(() => {
+                    // At 2200 ms the timeouts for 0 and 2 should have occurred
+                    // (not for 1), and the packets should have been resent
+                    sentPackets.should.have.lengthOf(5);
+                    const expectedSeqNums = [0, 1, 2, 0, 2];
+                    expectedSeqNums.forEach((seqNum, i) => {
+                        sentPackets[i].should.have.property("seqNum").equal(seqNum);
+                    })
+                    receivedOkAcks.should.have.lengthOf(1);
+                    receivedOkAcks[0].should.equal(acksToSend[1]);
+                    receivedNotOkAcks.should.have.lengthOf(2);
+                    receivedNotOkAcks[0].should.equal(acksToSend[0]);
+                    receivedNotOkAcks[1].should.equal(acksToSend[2]);
+                    // Stop traveling packets
+                    channel.stop();
+                    // Stop future timeouts
+                    sender.stop();
+                    done();
+                }, 2200);
+            })
 
-            it("should ignore corrupted packets");
+            it("should move the window when the oldest packet is acknowledged", function(done){
+                this.timeout(2500);
+                // We will try to send 4 packets, and as the window size is 3
+                // the fourth one should not be sent. Then we will correctly
+                // acknowledge the first and the third expecting the base
+                // sequence number of the window to become 1, and expecting
+                // to be able to send the fourth packet.
+                const ackSequences = [0, 0, 2]
+                ackSequences.forEach(ackNum => {
+                    acksToSend.push(new Packet({
+                        isAck: true,
+                        receiver: sender,
+                        ackNum
+                    }));
+                });
+                sender.should.have.property("base").equal(0);
+                sender.send();
+                sender.send();
+                sender.send();
+                // Trying to send the fourth packet and failing:
+                sender.send().should.be.false;
+                setTimeout(() => {
+                    sentPackets.should.have.lengthOf(3);
+                    receivedOkAcks.should.have.lengthOf(2);
+                    receivedNotOkAcks.should.have.lengthOf(1);
+                    sender.should.have.property("base").equal(1);
+                    sender.send().should.be.true;
+                    sentPackets.should.have.lengthOf(4);
+                    // Stop traveling packets
+                    channel.stop();
+                    // Prevent timeouts
+                    sender.stop();
+                    done();
+                }, 2200);
+            });
         });
-
     });
 
     describe("SRReceiver", function(){
